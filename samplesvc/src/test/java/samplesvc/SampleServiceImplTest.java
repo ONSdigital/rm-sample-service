e508325ab0d796b93c92ccbdacbb0802b07e90e0
Phil Whiles <phil.whiles@gmail.com>
2017-04-26 13:24:16 +0100

    Added XML and Party utils. So cool.

--- samplesvc/src/test/java/samplesvc/SampleServiceImplTest.java
+++ samplesvc/src/test/java/samplesvc/SampleServiceImplTest.java
@@ -1,44 +1,58 @@
 package samplesvc;
 
 import java.io.File;
-import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.XmlType;
 
 import org.junit.Test;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import uk.gov.ons.ctp.response.party.definition.Party;
 import uk.gov.ons.ctp.response.party.definition.PartyAttributeMap;
 import uk.gov.ons.ctp.response.sample.definition.BusinessSampleUnit;
 import uk.gov.ons.ctp.response.sample.definition.BusinessSurveySample;
+import uk.gov.ons.ctp.response.sample.definition.CensusSampleUnit;
+import uk.gov.ons.ctp.response.sample.definition.CensusSurveySample;
+import uk.gov.ons.ctp.response.sample.definition.SurveyBase;
+import uk.gov.ons.ctp.response.sample.party.PartyUtil;
+import uk.gov.ons.ctp.response.sample.xml.JaxbAnnotatedTypeUtil;
 
 public class SampleServiceImplTest {
 
   @Test
-  public void test() throws Exception {
+  public void testRemarshalCensus() throws Exception {
+    File file = new File("src/test/resources/uk/gov/ons/ctp/response/sample/service/impl/census-survey-sample.xml");
+    JAXBContext jaxbContext = JAXBContext.newInstance(SurveyBase.class);
+
+    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+    CensusSurveySample sample = (CensusSurveySample) jaxbUnmarshaller.unmarshal(file);
+    for (CensusSampleUnit unit : sample.getSampleUnits().getCensusSampleUnits()) {
+      Party p = PartyUtil.convertToParty(unit);
+      ObjectMapper mapper = new ObjectMapper();
+      System.out.println(mapper.writeValueAsString(p));
+    }
+  }
+
+  @Test
+  public void testRemarshalBusiness() throws Exception {
     File file = new File("src/test/resources/uk/gov/ons/ctp/response/sample/service/impl/business-survey-sample.xml");
     JAXBContext jaxbContext = JAXBContext.newInstance(BusinessSurveySample.class);
 
     Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
     BusinessSurveySample sample = (BusinessSurveySample) jaxbUnmarshaller.unmarshal(file);
     for (BusinessSampleUnit unit : sample.getSampleUnits().getBusinessSampleUnits()) {
-      Party p = new Party();
-      p.setSampleUnitType(unit.getSampleUnitType());
-      p.setSampleUnitRef(unit.getSampleUnitRef());
-      PartyAttributeMap attribs = new PartyAttributeMap();
-      attribs.put("businessRegion", unit.getBusinessRegion());
-      attribs.put("frosic2003", unit.getFrosic2003());
-      p.setAttributes(attribs);
-
+      Party p = PartyUtil.convertToParty(unit);
       ObjectMapper mapper = new ObjectMapper();
       System.out.println(mapper.writeValueAsString(p));
     }
   }
+
+
 }
