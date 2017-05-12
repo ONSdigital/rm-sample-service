9c6885853d691a4f7b36ba42c5da717a97a00d0c
Gareth Turner <gareth.turner@ons.gov.uk>
2017-05-12 10:44:22 +0000

    add test for json that fails validation

--- samplesvc/src/test/java/uk/gov/ons/ctp/response/sample/endpoint/SampleEndpointUnitTest.java
+++ samplesvc/src/test/java/uk/gov/ons/ctp/response/sample/endpoint/SampleEndpointUnitTest.java
@@ -24,15 +24,14 @@ import uk.gov.ons.ctp.common.FixtureHelper;
 import uk.gov.ons.ctp.common.error.CTPException;
 import uk.gov.ons.ctp.common.error.RestExceptionHandler;
 import uk.gov.ons.ctp.common.jackson.CustomObjectMapper;
-import uk.gov.ons.ctp.response.sample.domain.model.SampleSummary;
+import uk.gov.ons.ctp.response.sample.representation.CollectionExerciseJobCreationRequestDTO;
 import uk.gov.ons.ctp.response.sample.service.SampleService;
 
 public class SampleEndpointUnitTest{
   private static final String SAMPLE_VALIDJSON = "{ \"collectionExerciseId\" : \"1\", \"surveyRef\" : \"str1234\", \"exerciseDateTime\" : \"2012-12-13T12:12:12.000+0000\" }";
   private static final String SAMPLE_INVALIDJSON1 = "{ \"collectionExerciseId\" : \"2\", \"surveyRef\" : \"str1234\", \"exerciseDateTime\" : \"201.000+0000\" }";
-  private static final String SAMPLE_INVALIDJSON2 = "{ \"collectionId\" : \"3\", \"surveyRef\" : \"str1234\", \"exerciseDateTime\" : \"2012-12-13T12:12:12.000+0000\" }";
+  private static final String SAMPLE_INVALIDJSON2 = "{ \"collectionExerciseId\" : \"\", \"surveyRef\" : \"str1234\", \"exerciseDateTime\" : \"2012-12-13T12:12:12.000+0000\" }";
   
-  private static final Integer SAMPLE_ID = 124;
   
   @InjectMocks
   private SampleEndpoint sampleEndpoint;
@@ -42,7 +41,8 @@ public class SampleEndpointUnitTest{
   
   
   private MockMvc mockMvc;
-  private List<SampleSummary> sampleSummaryResults;
+  private List<CollectionExerciseJobCreationRequestDTO> collectionExerciseRequests;
+
   
   @Before
   public void setUp() throws Exception {
@@ -53,27 +53,39 @@ public class SampleEndpointUnitTest{
         .setMessageConverters(new MappingJackson2HttpMessageConverter(new CustomObjectMapper()))
         .build();
     
-    this.sampleSummaryResults = FixtureHelper.loadClassFixtures(SampleSummary[].class);
+    this.collectionExerciseRequests = FixtureHelper.loadClassFixtures(CollectionExerciseJobCreationRequestDTO[].class);
+
   }
   
   @Test
   public void getSampleSummaryValidJSON() throws Exception{
-    //when(sampleService.findSampleSummaryBySampleId(SAMPLE_ID)).thenReturn(sampleSummaryResults.get(0));
+    when(sampleService.initialiseCollectionExerciseJob(collectionExerciseRequests.get(0))).thenReturn(4);
      
     ResultActions actions = mockMvc.perform(postJson(String.format("/samples/sampleunitrequests"), SAMPLE_VALIDJSON));
     
     actions.andExpect(status().isCreated());
+    actions.andExpect(jsonPath("$.sampleUnitsTotal", is(4)));
   }
   
 
   @Test
-  public void acknowledgeReceiptBadJsonProvidedScenario2() throws Exception {
+  public void acknowledgeReceiptBadJsonProvidedScenario1() throws Exception {
     ResultActions actions = mockMvc.perform(postJson(String.format("/samples/sampleunitrequests"), SAMPLE_INVALIDJSON1));
 
     actions.andExpect(status().isBadRequest())
         .andExpect(jsonPath("$.error.code", is(CTPException.Fault.VALIDATION_FAILED.name())))
         .andExpect(jsonPath("$.error.timestamp", isA(String.class)))
         .andExpect(jsonPath("$.error.message", is("Provided json is incorrect.")));
+  }
+  
+  @Test
+  public void acknowledgeReceiptBadJsonProvidedScenario2() throws Exception {
+    ResultActions actions = mockMvc.perform(postJson(String.format("/samples/sampleunitrequests"), SAMPLE_INVALIDJSON2));
+
+    actions.andExpect(status().isBadRequest())
+        .andExpect(jsonPath("$.error.code", is(CTPException.Fault.VALIDATION_FAILED.name())))
+        .andExpect(jsonPath("$.error.timestamp", isA(String.class)))
+        .andExpect(jsonPath("$.error.message", is("Provided json fails validation.")));
   }
   
 }
