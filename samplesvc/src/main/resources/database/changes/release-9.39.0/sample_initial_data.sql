4ddb4da1cf5100c2da5c2e579f6fcb25a7bc45a5
Narinder Birk <narinderbirk@ip-172-29-35-233.eu-west-1.compute.internal>
2017-05-24 13:38:11 +0100

    CTPA-1285 - samplesvc database changes

---
+++ samplesvc/src/main/resources/database/changes/release-9.39.0/sample_initial_data.sql
@@ -0,0 +1,7 @@
+SET SCHEMA 'sample';
+
+INSERT INTO sample.samplesummarystate(state) VALUES('INIT');
+INSERT INTO sample.samplesummarystate(state) VALUES('ACTIVE');
+
+INSERT INTO sample.sampleunitstate(state) VALUES('INIT');
+INSERT INTO sample.sampleunitstate(state) VALUES('DELIVERED');
