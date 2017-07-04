5af7bd5ee2bd0f5e4d17bff72c7d913efb72c951
KieranWardle <kieran.wardle@ons.gov.uk>
2017-07-04 12:36:47 +0100

    CTPA-1367 Added @cobaturaIgnore annotation to all config classes

--- samplesvc/src/main/java/uk/gov/ons/ctp/response/sample/config/DataGrid.java
+++ samplesvc/src/main/java/uk/gov/ons/ctp/response/sample/config/DataGrid.java
@@ -1,11 +1,13 @@
 package uk.gov.ons.ctp.response.sample.config;
 
 import lombok.Data;
+import net.sourceforge.cobertura.CoverageIgnore;
 
 /**
  * Config POJO for action plan exec params
  *
  */
+@CoverageIgnore
 @Data
 public class DataGrid {
   private String address;
--- samplesvc/src/main/java/uk/gov/ons/ctp/response/sample/config/SampleSvc.java
+++ samplesvc/src/main/java/uk/gov/ons/ctp/response/sample/config/SampleSvc.java
@@ -1,6 +1,7 @@
 package uk.gov.ons.ctp.response.sample.config;
 
 import lombok.Data;
+import net.sourceforge.cobertura.CoverageIgnore;
 import uk.gov.ons.ctp.common.rest.RestClientConfig;
 
 /**
@@ -8,6 +9,7 @@ import uk.gov.ons.ctp.common.rest.RestClientConfig;
  * Application config bean for the connection details to the Sample Service.
  *
  */
+@CoverageIgnore
 @Data
 public class SampleSvc {
     private RestClientConfig connectionConfig;
