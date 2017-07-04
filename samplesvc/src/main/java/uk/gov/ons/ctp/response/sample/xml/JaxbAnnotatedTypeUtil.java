3795fce5538feee8d762aca69536ab68face8105
KieranWardle <kieran.wardle@ons.gov.uk>
2017-07-04 11:30:30 +0100

    CTPA-1367 Made library class unconstructible to silence cobertura

--- samplesvc/src/main/java/uk/gov/ons/ctp/response/sample/xml/JaxbAnnotatedTypeUtil.java
+++ samplesvc/src/main/java/uk/gov/ons/ctp/response/sample/xml/JaxbAnnotatedTypeUtil.java
@@ -11,6 +11,7 @@ import java.util.Map;
  */
 public final class JaxbAnnotatedTypeUtil {
 
+  private JaxbAnnotatedTypeUtil(){}
   /**
    * Takes the given object and extracts using reflection the @XmlType annotated
    * properties, returning a map where the keys are the property names and the
