7a924d8fcee1f7bc970a0e86f2dc8f39e63ecbf7
KieranWardle <kieran.wardle@ons.gov.uk>
2017-07-06 13:46:28 +0100

    CTPA-1367 Tidied up test file

--- samplesvc/src/test/java/uk/gov/ons/ctp/response/sample/service/impl/SampleServiceImplTest.java
+++ samplesvc/src/test/java/uk/gov/ons/ctp/response/sample/service/impl/SampleServiceImplTest.java
@@ -1,31 +1,22 @@
 package uk.gov.ons.ctp.response.sample.service.impl;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 import uk.gov.ons.ctp.common.FixtureHelper;
-import uk.gov.ons.ctp.common.rest.RestClient;
-import uk.gov.ons.ctp.response.sample.config.AppConfig;
-import uk.gov.ons.ctp.response.sample.definition.BusinessSampleUnit;
 import uk.gov.ons.ctp.response.sample.definition.SampleUnitBase;
 import uk.gov.ons.ctp.response.sample.definition.SurveyBase;
 import uk.gov.ons.ctp.response.sample.domain.model.SampleSummary;
 import uk.gov.ons.ctp.response.sample.domain.model.SampleUnit;
-import uk.gov.ons.ctp.response.sample.domain.repository.SampleSummaryRepository;
 import uk.gov.ons.ctp.response.sample.representation.SampleSummaryDTO;
 
-import java.sql.Timestamp;
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static uk.gov.ons.ctp.common.TestHelper.createTestDate;
 
 
 /**
@@ -37,24 +28,21 @@ public class SampleServiceImplTest {
     @InjectMocks
     private SampleServiceImpl sampleServiceImpl;
 
-    @Mock
-    private SampleSummaryRepository sampleSummaryRepository;
+    private List<SurveyBase> surveySample;
+    private List<SampleUnitBase> sampleUnitList;
 
-    @Mock
-    private RestClient partySvcClient;
-
-    @Mock
-    private AppConfig appConfig;
-
+    @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+        surveySample = FixtureHelper.loadClassFixtures(SurveyBase[].class);
+        sampleUnitList = FixtureHelper.loadClassFixtures(SampleUnitBase[].class);
+
     }
 
     private static final int SAMPLE_SUMMARY_PK = 1;
 
     @Test
     public void verifySampleSummaryCreatedCorrectly() throws Exception {
-        List<SurveyBase> surveySample = FixtureHelper.loadClassFixtures(SurveyBase[].class);
         SampleSummary sampleSummary = sampleServiceImpl.createSampleSummary(surveySample.get(0));
         assertTrue(sampleSummary.getSurveyRef().equals("abc"));
         assertNotNull(sampleSummary.getIngestDateTime());
@@ -65,30 +53,13 @@ public class SampleServiceImplTest {
 
     @Test
     public void verifySampleUnitsCreatedCorrectly() throws Exception {
-        List<SurveyBase> surveySample = FixtureHelper.loadClassFixtures(SurveyBase[].class);
         SampleSummary sampleSummary = sampleServiceImpl.createSampleSummary(surveySample.get(0));
         sampleSummary.setSampleSummaryPK(SAMPLE_SUMMARY_PK);
-        List<SampleUnitBase> sampleUnitBase = FixtureHelper.loadClassFixtures(SampleUnitBase[].class);
-        SampleUnit sampleUnit = sampleServiceImpl.createSampleUnit(sampleSummary, sampleUnitBase.get(0));
+        SampleUnit sampleUnit = sampleServiceImpl.createSampleUnit(sampleSummary, sampleUnitList.get(0));
         assertTrue(sampleUnit.getFormType().equals("112"));
         assertTrue(sampleUnit.getSampleUnitRef().equals("abc"));
         assertTrue(sampleUnit.getSampleUnitType().equals("B"));
         assertTrue(sampleUnit.getSampleSummaryFK() == SAMPLE_SUMMARY_PK);
-    }
-
-    @Test
-    public void verifySampleUnitsSentToParty() {
-
-    }
-
-    @Test
-    public void verifyBadSampleUnitsRejectedByParty() {
-
-    }
-
-    @Test
-    public void verifySampleUnitsStateChangedToActive() {
-
     }
 
 }
