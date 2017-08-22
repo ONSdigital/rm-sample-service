2f060cb5e47d194784d7cd4de5ce427d3ce84d8a
Chris Hardman <christopher.hardman@ons.gov.uk>
2017-08-22 13:43:29 +0100

    CTPA-1517: Sample now generates a Report when a Sample file is ingested

---
+++ src/main/java/uk/gov/ons/ctp/response/sample/service/SampleReportService.java
@@ -0,0 +1,12 @@
+package uk.gov.ons.ctp.response.sample.service;
+
+/**
+ * The service to create reports
+ */
+public interface SampleReportService {
+
+    /**
+     * To create reports
+     */
+    void createReport();
+}
