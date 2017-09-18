acf3d8727df6ca05bffd5ed35e99d61c67ad25ce
Edward Stevens <edward.stevens@ons.gov.uk>
2017-09-18 16:34:59 +0100

    Codacy Fixes

--- src/test/java/samplesvc/SampleServiceImplTest.java
+++ src/test/java/samplesvc/SampleServiceImplTest.java
@@ -16,6 +16,9 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 
+/**
+ * Tests remarshalling for Census, Business and Social
+ */
 public class SampleServiceImplTest {
 
   /**
