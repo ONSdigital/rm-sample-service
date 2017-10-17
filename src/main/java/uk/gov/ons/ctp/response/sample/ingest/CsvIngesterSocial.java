22cf75ff9da9e7fd354b78ced6bc7a4c84f66e24
Edward Stevens <edward.stevens@ons.gov.uk>
2017-10-17 14:57:15 +0100

    CTPA-1632 modified for social survey ingest

--- src/main/java/uk/gov/ons/ctp/response/sample/ingest/CsvIngesterSocial.java
+++ src/main/java/uk/gov/ons/ctp/response/sample/ingest/CsvIngesterSocial.java
@@ -30,44 +30,10 @@ import java.util.stream.Collectors;
 @Service
 public class CsvIngesterSocial extends CsvToBean<SocialSampleUnit> {
 
-  private static final String SURVEYREF = "surveyRef";
-  private static final String COLLECTIONEXERCISEREF = "collectionExerciseRef";
-  private static final String EFFECTIVESTARTDATETIME = "effectiveStartDateTime";
-  private static final String EFFECTIVEENDDATETIME = "effectiveEndDateTime";
   private static final String SAMPLEUNITREF = "sampleUnitRef";
-  private static final String SAMPLEUNITTYPE = "sampleUnitType";
   private static final String FORMTYPE = "formType";
-  private static final String CHECKLETTER = "checkletter";
-  private static final String FROSIC92 = "frosic92";
-  private static final String RUSIC92 = "rusic92";
-  private static final String FROSIC2007 = "frosic2007";
-  private static final String RUSIC2007 = "rusic2007";
-  private static final String FROEMPMENT = "froempment";
-  private static final String FROTOVER = "frotover";
-  private static final String ENTREF = "entref";
-  private static final String LEGALSTATUS = "legalstatus";
-  private static final String ENTREPMKR = "entrepmkr";
-  private static final String REGION = "region";
-  private static final String BIRTHDATE = "birthdate";
-  private static final String ENTNAME1 = "entname1";
-  private static final String ENTNAME2 = "entname2";
-  private static final String ENTNAME3 = "entname3";
-  private static final String RUNAME1 = "runame1";
-  private static final String RUNAME2 = "runame2";
-  private static final String RUNAME3 = "runame3";
-  private static final String TRADSTYLE1 = "tradstyle1";
-  private static final String TRADSTYLE2 = "tradstyle2";
-  private static final String TRADSTYLE3 = "tradstyle3";
-  private static final String SELTYPE = "seltype";
-  private static final String INCLEXCL = "inclexcl";
-  private static final String CELLNO = "cell_no";
-  private static final String FORMTYPE2 = "formtype2";
-  private static final String CURRENCY = "currency";
 
-  private static final String[] COLUMNS = new String[] {SURVEYREF, COLLECTIONEXERCISEREF, EFFECTIVESTARTDATETIME, EFFECTIVEENDDATETIME, SAMPLEUNITREF, SAMPLEUNITTYPE, FORMTYPE, CHECKLETTER, FROSIC92, RUSIC92, FROSIC2007, RUSIC2007,
-      FROEMPMENT, FROTOVER, ENTREF, LEGALSTATUS, ENTREPMKR, REGION, BIRTHDATE, ENTNAME1, ENTNAME2, ENTNAME3,
-      RUNAME1, RUNAME2, RUNAME3, TRADSTYLE1, TRADSTYLE2, TRADSTYLE3, SELTYPE, INCLEXCL, CELLNO, FORMTYPE2,
-      CURRENCY};
+  private static final String[] COLUMNS = new String[] {SAMPLEUNITREF, FORMTYPE};
 
   @Autowired
   private SampleService sampleService;
@@ -99,18 +65,9 @@ public class CsvIngesterSocial extends CsvToBean<SocialSampleUnit> {
     SampleSummary sampleSummary;
     SocialSurveySample businessSurveySample = new SocialSurveySample();
     List<SocialSampleUnit> samplingUnitList = new ArrayList<>();
-    int lineNum = 0;
 
       while((nextLine = csvReader.readNext()) != null) {
 
-        if (lineNum == 1) {
-          businessSurveySample.setSurveyRef(nextLine[0]);
-          businessSurveySample.setCollectionExerciseRef(nextLine[1]);
-          businessSurveySample.setEffectiveStartDateTime(nextLine[2]);
-          businessSurveySample.setEffectiveEndDateTime(nextLine[3]);
-        }
-
-        if (lineNum++ > 0) {
           SocialSampleUnit businessSampleUnit = processLine(columnPositionMappingStrategy, nextLine);
           Optional<String> namesOfInvalidColumns = validateLine(businessSampleUnit);
           if (namesOfInvalidColumns.isPresent()) {
@@ -121,8 +78,6 @@ public class CsvIngesterSocial extends CsvToBean<SocialSampleUnit> {
           }
 
           samplingUnitList.add(businessSampleUnit);
-
-        }
 
       }
 
---
+++ src/test/java/uk/gov/ons/ctp/response/sample/endpoint/CsvIngesterSocialTest.java
@@ -0,0 +1,94 @@
+package uk.gov.ons.ctp.response.sample.endpoint;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.mock.web.MockMultipartFile;
+import uk.gov.ons.ctp.common.error.CTPException;
+import uk.gov.ons.ctp.response.sample.config.AppConfig;
+import uk.gov.ons.ctp.response.sample.ingest.CsvIngesterSocial;
+import uk.gov.ons.ctp.response.sample.service.SampleService;
+import validation.SocialSampleUnit;
+import validation.SocialSurveySample;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test the CsvIngester distributor
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class CsvIngesterSocialTest {
+
+  @Spy
+  private AppConfig appConfig = new AppConfig();
+
+  @InjectMocks
+  private CsvIngesterSocial csvIngester;
+
+  @InjectMocks
+  private SampleEndpoint sampleEndpoint;
+
+  @Mock
+  private SampleService sampleService;
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  /**
+   * take a named test file and create a copy of it - is because the ingester
+   * will delete the source csv file after ingest
+   *
+   * @param fileName source file name
+   * @return the newly created file
+   * @throws Exception oops
+   */
+  private MockMultipartFile getTestFile(String fileName) throws Exception {
+    Path csvFileLocation = Paths.get(getClass().getClassLoader().getResource("csv/" + fileName).toURI());
+    MockMultipartFile multipartFile = new MockMultipartFile("file", fileName, "csv",
+        Files.readAllBytes(csvFileLocation));
+    return multipartFile;
+  }
+
+  @Test
+  public void testBlueSky() throws Exception {
+    csvIngester.ingest(getTestFile("social-survey-sample.csv"));
+    verify(sampleService, times(1)).processSampleSummary(any(SocialSurveySample.class),
+        anyListOf(SocialSampleUnit.class));
+  }
+
+  @Test(expected = CTPException.class)
+  public void missingColumns() throws Exception {
+    csvIngester.ingest(getTestFile("business-survey-sample-missing-columns.csv"));
+    verify(sampleService, times(0)).processSampleSummary(any(SocialSurveySample.class),
+        anyListOf(SocialSampleUnit.class));
+    thrown.expect(CTPException.class);
+  }
+
+  @Test(expected = CTPException.class)
+  public void incorrectData() throws Exception {
+    csvIngester.ingest(getTestFile("business-survey-sample-incorrect-data.csv"));
+    verify(sampleService, times(0)).processSampleSummary(any(SocialSurveySample.class),
+        anyListOf(SocialSampleUnit.class));
+    thrown.expect(CTPException.class);
+  }
+
+}
