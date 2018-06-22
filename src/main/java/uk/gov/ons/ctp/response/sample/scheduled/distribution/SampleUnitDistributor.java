c973a4d3bd9c0f222f97284ef1304e0b7d4e3311
damien-lloyd <damien.lloyd21@gmail.com>
2018-06-22 12:25:30 +0100

    Fixed failing test due to type issue from UUID to String.

--- src/main/java/uk/gov/ons/ctp/response/sample/scheduled/distribution/SampleUnitDistributor.java
+++ src/main/java/uk/gov/ons/ctp/response/sample/scheduled/distribution/SampleUnitDistributor.java
@@ -99,6 +99,7 @@ public class SampleUnitDistributor {
                 uk.gov.ons.ctp.response.sampleunit.definition.SampleUnit.class);
             uk.gov.ons.ctp.response.sample.domain.model.SampleAttributes sampleAttributes = sampleAttributesRepository.findOne(sampleUnit.getId());
             if (sampleAttributes != null) {
+              mappedSampleUnit.setId(sampleUnit.getId().toString());
               mappedSampleUnit.setSampleAttributes(mapSampleAttributes(sampleAttributes));
             }
             mappedSampleUnit.setCollectionExerciseId(job.getCollectionExerciseId().toString());
