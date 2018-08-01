a0b49739e6cf002cf700ec2601c39e465aaf57f3
damien-lloyd <damien.lloyd21@gmail.com>
2018-08-01 15:09:15 +0100

    Added query for getting sample attributes by post code

--- src/main/java/uk/gov/ons/ctp/response/sample/domain/repository/SampleAttributesRepository.java
+++ src/main/java/uk/gov/ons/ctp/response/sample/domain/repository/SampleAttributesRepository.java
@@ -2,9 +2,14 @@ package uk.gov.ons.ctp.response.sample.domain.repository;
 
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import uk.gov.ons.ctp.response.sample.domain.model.SampleAttributes;
 
 /** JPA Data Repository needed to persist Survey SampleAttributes */
 @Repository
-public interface SampleAttributesRepository extends JpaRepository<SampleAttributes, UUID> {}
+public interface SampleAttributesRepository extends JpaRepository<SampleAttributes, UUID> {
+  
+  @Query("select * from sample.sampleattributes where REPLACE(attributes->>'Postcode', ' ', '') = REPLACE(':postcode', ' ', '')")
+  public SampleAttributes findByPostcode(String postcode);
+}
