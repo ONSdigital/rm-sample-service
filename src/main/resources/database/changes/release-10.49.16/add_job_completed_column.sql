96dd73456752a9dafc8ac2672ce06f2dcce79a5d
Nick Grant <nick.grant@ext.ons.gov.uk>
2018-08-09 10:58:05 +0100

    Set existing rows to jobcomplete = true

--- src/main/resources/database/changes/release-10.49.16/add_job_completed_column.sql
+++ src/main/resources/database/changes/release-10.49.16/add_job_completed_column.sql
@@ -1,4 +1,4 @@
 ALTER TABLE sample.collectionexercisejob ADD COLUMN jobcomplete boolean;
-UPDATE sample.collectionexercisejob SET jobcomplete = 'f';
+UPDATE sample.collectionexercisejob SET jobcomplete = 't';
 ALTER TABLE sample.collectionexercisejob ALTER COLUMN jobcomplete SET NOT NULL;
 ALTER TABLE sample.collectionexercisejob ALTER COLUMN jobcomplete SET DEFAULT FALSE;
