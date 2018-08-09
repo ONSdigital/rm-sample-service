504e686e9e32ffb3d338fd5d67004362b01c7dfa
Nick Grant <nick.grant@ext.ons.gov.uk>
2018-08-09 17:25:33 +0100

    Belt and braces check just before we send to rabbit

--- src/main/java/uk/gov/ons/ctp/response/sample/scheduled/distribution/SampleUnitSender.java
+++ src/main/java/uk/gov/ons/ctp/response/sample/scheduled/distribution/SampleUnitSender.java
@@ -40,16 +40,22 @@ public class SampleUnitSender {
   /** Send a SampleUnit */
   @Transactional(propagation = Propagation.REQUIRES_NEW)
   public void sendSampleUnit(SampleUnit mappedSampleUnit) throws CTPException {
-    // Send to Rabbit queue
-    sampleUnitPublisher.send(mappedSampleUnit);
-
-    // Because we've now successfully queued the message we can change the state in the DB
     uk.gov.ons.ctp.response.sample.domain.model.SampleUnit sampleUnit =
         sampleUnitRepository.findById(UUID.fromString(mappedSampleUnit.getId()));
-    SampleUnitDTO.SampleUnitState newState =
-        sampleUnitStateTransitionManager.transition(
-            sampleUnit.getState(), SampleUnitDTO.SampleUnitEvent.DELIVERING);
-    sampleUnit.setState(newState);
-    sampleUnitRepository.saveAndFlush(sampleUnit);
+
+    // Highly unlikely, but do a quick (and inexpensive) check to make sure we haven't already
+    // delivered this Sample Unit - could happen if there's a problem with the locking/Redis
+    // timing out and releasing the lock due to the delivery process taking a long time.
+    if (sampleUnit.getState() == SampleUnitState.PERSISTED) {
+      // Send to Rabbit queue
+      sampleUnitPublisher.send(mappedSampleUnit);
+
+      // Because we've now successfully queued the message we can change the state in the DB
+      SampleUnitDTO.SampleUnitState newState =
+          sampleUnitStateTransitionManager.transition(
+              sampleUnit.getState(), SampleUnitDTO.SampleUnitEvent.DELIVERING);
+      sampleUnit.setState(newState);
+      sampleUnitRepository.saveAndFlush(sampleUnit);
+    }
   }
 }
