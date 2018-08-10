36b8dba1a864a81846a4820b30208275ca7c469a
Nick Grant <nick.grant@ext.ons.gov.uk>
2018-08-10 11:09:42 +0100

    Fix for unlikely edge case where job was in-flight when upgrade happened

--- src/main/resources/database/changes/release-11/add_job_completed_column.sql
+++ src/main/resources/database/changes/release-11/add_job_completed_column.sql
@@ -1,4 +1,8 @@
 ALTER TABLE sample.collectionexercisejob ADD COLUMN jobcomplete boolean;
-UPDATE sample.collectionexercisejob SET jobcomplete = 't';
+
+-- Mark all the existing jobs as "not complete" so that anything that was in-flight when the upgrade
+-- is run will be safely resumed and the service will eventually get back to a consistent state
+UPDATE sample.collectionexercisejob SET jobcomplete = 'f';
+
 ALTER TABLE sample.collectionexercisejob ALTER COLUMN jobcomplete SET NOT NULL;
 ALTER TABLE sample.collectionexercisejob ALTER COLUMN jobcomplete SET DEFAULT FALSE;
