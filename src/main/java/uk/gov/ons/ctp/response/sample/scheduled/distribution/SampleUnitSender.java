c2cecdb7914812b1b5ced39ca09b9e360dacac47
jterryons <john.terry@ons.gov.uk>
2020-08-11 21:02:32 +0100

    Revert "remove prsisted check"
    This reverts commit e00dbd5cf29b7be9fb192a880d7adbd59063560d.

--- src/main/java/uk/gov/ons/ctp/response/sample/scheduled/distribution/SampleUnitSender.java
+++ src/main/java/uk/gov/ons/ctp/response/sample/scheduled/distribution/SampleUnitSender.java
@@ -45,14 +45,19 @@ public class SampleUnitSender {
     uk.gov.ons.ctp.response.sample.domain.model.SampleUnit sampleUnit =
         sampleUnitRepository.findById(UUID.fromString(mappedSampleUnit.getId()));
 
-    // Send to Rabbit queue
-    sampleUnitPublisher.send(mappedSampleUnit);
+    // Highly unlikely, but do a quick (and inexpensive) check to make sure we haven't already
+    // delivered this Sample Unit - could happen if there's a problem with the locking/Redis
+    // timing out and releasing the lock due to the delivery process taking a long time.
+    if (sampleUnit.getState() == SampleUnitState.PERSISTED) {
+      // Send to Rabbit queue
+      sampleUnitPublisher.send(mappedSampleUnit);
 
-    // Because we've now successfully queued the message we can change the state in the DB
-    SampleUnitDTO.SampleUnitState newState =
-        sampleUnitStateTransitionManager.transition(
-            sampleUnit.getState(), SampleUnitDTO.SampleUnitEvent.DELIVERING);
-    sampleUnit.setState(newState);
-    sampleUnitRepository.saveAndFlush(sampleUnit);
+      // Because we've now successfully queued the message we can change the state in the DB
+      SampleUnitDTO.SampleUnitState newState =
+          sampleUnitStateTransitionManager.transition(
+              sampleUnit.getState(), SampleUnitDTO.SampleUnitEvent.DELIVERING);
+      sampleUnit.setState(newState);
+      sampleUnitRepository.saveAndFlush(sampleUnit);
+    }
   }
 }
