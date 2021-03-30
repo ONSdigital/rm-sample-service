cd5de97a88bca4352f438137907f1098e88b0f2d
Matt <matt.crocker@gmail.com>
2021-03-30 16:38:24 +0100

    Upgrade to Spring Boot 2.4.4

--- src/test/java/uk/gov/ons/ctp/response/sample/scheduled/distribution/SampleUnitMapperTest.java
+++ src/test/java/uk/gov/ons/ctp/response/sample/scheduled/distribution/SampleUnitMapperTest.java
@@ -10,7 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.ons.ctp.response.sample.SampleBeanMapper;
 import uk.gov.ons.ctp.response.sample.domain.model.SampleUnit;
 
