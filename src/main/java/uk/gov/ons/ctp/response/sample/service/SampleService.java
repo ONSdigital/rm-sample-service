c2d8e2c01ebbb8c2bb0e202592a990b6f6cfb77a
Warren Bailey <warren@warrenbailey.net>
2021-05-20 15:27:30 +0100

    Re-introduce saving party id

--- src/main/java/uk/gov/ons/ctp/response/sample/service/SampleService.java
+++ src/main/java/uk/gov/ons/ctp/response/sample/service/SampleService.java
@@ -198,11 +198,18 @@ public class SampleService {
           sampleUnitRepository.findById(UUID.fromString(sampleUnitId)).orElseThrow();
       changeSampleUnitState(sampleUnit);
       sampleSummaryStateCheck(sampleUnit);
+      addPartyIdToSample(sampleUnit, returnedParty);
       return returnedParty;
     } catch (NoSuchElementException e) {
       log.error("unable to find sample ", kv("sampleUnitId", sampleUnitId));
       throw new CTPException(CTPException.Fault.RESOURCE_NOT_FOUND);
     }
+  }
+
+  private void addPartyIdToSample(SampleUnit sampleUnit, PartyDTO party) {
+    UUID partyId = UUID.fromString(party.getId());
+    sampleUnit.setPartyId(partyId);
+    sampleUnitRepository.saveAndFlush(sampleUnit);
   }
 
   private void changeSampleUnitState(SampleUnit sampleUnit) throws CTPException {
