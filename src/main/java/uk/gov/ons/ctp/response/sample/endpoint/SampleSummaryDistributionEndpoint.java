87f2779256efaf18c5c368cf84055f7c9bab91bb
Adam Mann <manny160389@hotmail.com>
2021-08-04 14:43:53 +0100

    Checkin progress

---
+++ src/main/java/uk/gov/ons/ctp/response/sample/endpoint/SampleSummaryDistributionEndpoint.java
@@ -0,0 +1,116 @@
+package uk.gov.ons.ctp.response.sample.endpoint;
+
+import libs.common.error.CTPException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.ons.ctp.response.sample.domain.model.SampleUnit;
+import uk.gov.ons.ctp.response.sample.message.SampleUnitPublisher;
+import uk.gov.ons.ctp.response.sample.service.SampleService;
+import uk.gov.ons.ctp.response.sample.service.SampleSummaryEnrichmentService;
+import uk.gov.ons.ctp.response.sample.service.UnknownSampleSummaryException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
+@RestController
+@RequestMapping(value = "/distribute", produces = "application/json")
+public class SampleSummaryDistributionEndpoint {
+
+  private static final Logger LOG = LoggerFactory.getLogger(SampleSummaryDistributionEndpoint.class);
+
+  @Autowired private SampleSummaryEnrichmentService sampleSummaryEnrichmentService;
+  @Autowired private SampleService sampleService;
+  @Autowired private SampleUnitPublisher sampleUnitPublisher;
+
+  @RequestMapping(
+      value =
+          "/survey/{surveyId}/collection-exercise/{collectionExerciseId}/samplesummary/{sampleSummaryId}",
+      method = RequestMethod.GET)
+  public ResponseEntity<Void> distribute(
+      @PathVariable("surveyId") String surveyId,
+      @PathVariable("collectionExerciseId") String collectionExerciseId,
+      @PathVariable("sampleSummaryId") UUID sampleSummaryId) {
+
+    LOG.info("Distributing sample unit",
+             kv("collectionExerciseId", collectionExerciseId));
+
+    // Don't need to get a sample unit group as it's a 1-1 mapping between sample units.  We can just get a list of
+    // all the sample units for a sample summary id and go from there.
+
+    List<SampleUnit> sampleUnits = sampleService.findSampleUnitsBySampleSummary(sampleSummaryId);
+
+    if (sampleUnits.isEmpty()) {
+      LOG.info("No sample unit groups to distribute for exercise",
+              kv("collectionExerciseId", collectionExerciseId));
+      return ResponseEntity.badRequest().build();
+    }
+
+    // Catch errors distributing sample units so that only failing units are stopped
+    sampleUnits.forEach(
+            sampleUnit -> {
+              try {
+                distributeSampleUnitGroup(exercise, sampleUnit);
+              } catch (CTPException ex) {
+                log.with("sampleUnitGroupPK", sampleUnitGroup.getSampleUnitGroupPK())
+                        .error("Failed to distribute sample unit group", ex);
+              }
+            });
+
+    return ResponseEntity.ok().build();
+  }
+
+  /**
+   * Distribute SampleUnits for a SampleUnitGroup. Will send the sampleUnitParent data to Case via
+   * PubSub and will transition the sampleUnitGroup state in collection exercise to PUBLISHED on
+   * success.
+   *
+   * @param exercise CollectionExercise of which sampleUnitGroup is a member
+   * @param sampleUnitGroup for which to distribute sample units
+   */
+  private void distributeSampleUnitGroup(
+          CollectionExercise exercise, SampleUnit sampleUnit) throws CTPException {
+    SampleUnitParentDTO sampleUnitParent;
+
+    boolean activeEnrolment = sampleUnit.isActiveEnrolment();
+    sampleUnitParent = sampleUnit.toSampleUnitParent(activeEnrolment, exercise.getId());
+
+    publishSampleUnitToCase(sampleUnitGroup, sampleUnitParent);
+  }
+
+  /**
+   * Publish a message to the Case Service for a SampleUnitGroup and transition state. Note this is
+   * a transaction boundary but as a private method we cannot use Spring declarative transaction
+   * management but must use a programmatic transaction.
+   *
+   * @param sampleUnitGroup from which publish message created and for which to transition state.
+   * @param sampleUnitMessage to publish.
+   */
+  private void publishSampleUnitToCase(
+          ExerciseSampleUnitGroup sampleUnitGroup, SampleUnitParentDTO sampleUnitMessage) {
+
+    transactionTemplate.execute(
+            new TransactionCallbackWithoutResult() {
+              // the code in this method executes in a transaction context
+              protected void doInTransactionWithoutResult(TransactionStatus status) {
+                try {
+                  sampleUnitGroupTransitionState(sampleUnitGroup);
+                  sampleUnitPublisher.sendSampleUnitToCase(sampleUnitMessage);
+                } catch (CTPException ex) {
+                  log.error("Sample Unit group state transition failed", ex);
+                }
+              }
+            });
+  }
+}
--- src/main/java/uk/gov/ons/ctp/response/sample/message/SampleUnitPublisher.java
+++ src/main/java/uk/gov/ons/ctp/response/sample/message/SampleUnitPublisher.java
@@ -77,4 +77,49 @@ public class SampleUnitPublisher {
       throw new RuntimeException(e);
     }
   }
+
+  public void sendSampleUnitToCase(SampleUnitParentDTO sampleUnit) {
+    log.with("sample_unit_ref", sampleUnit.getSampleUnitRef())
+            .with("sample_unit_type", sampleUnit.getSampleUnitType())
+            .debug("Entering sendSampleUnit");
+    try {
+      String message = objectMapper.writeValueAsString(sampleUnit);
+      ByteString data = ByteString.copyFromUtf8(message);
+      PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
+      Publisher publisher = pubSub.sampleUnitPublisher();
+      try {
+        log.with("publisher", publisher).info("Publishing message to PubSub");
+        ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
+        ApiFutures.addCallback(
+                messageIdFuture,
+                new ApiFutureCallback<>() {
+                  @Override
+                  public void onFailure(Throwable throwable) {
+                    if (throwable instanceof ApiException) {
+                      ApiException apiException = ((ApiException) throwable);
+                      log.with("error", apiException.getStatusCode().getCode())
+                              .error("SampleUnit publish sent failure to PubSub.");
+                    }
+                    log.with("message", message).error("Error Publishing PubSub message");
+                  }
+
+                  @Override
+                  public void onSuccess(String messageId) {
+                    // Once published, returns server-assigned message ids (unique within the topic)
+                    log.with("messageId", messageId).info("SampleUnit publish sent successfully");
+                  }
+                },
+                MoreExecutors.directExecutor());
+      } finally {
+        publisher.shutdown();
+        pubSub.shutdown();
+      }
+    } catch (JsonProcessingException e) {
+      log.with("sampleUnit", sampleUnit).error("Error while sampleUnit can not be parsed.");
+      throw new RuntimeException(e);
+    } catch (IOException e) {
+      log.error("PubSub Error while processing sample unit distribution", e);
+      throw new RuntimeException(e);
+    }
+  }
 }
