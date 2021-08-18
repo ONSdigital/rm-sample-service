8a648d458d756f7119f52ea6ee0cd45145996f13
Adam Mann <manny160389@hotmail.com>
2021-08-18 17:54:45 +0100

    Add tests

--- src/test/java/uk/gov/ons/ctp/response/sample/service/SampleSummaryActivationServiceTest.java
+++ src/test/java/uk/gov/ons/ctp/response/sample/service/SampleSummaryActivationServiceTest.java
@@ -3,12 +3,15 @@ package uk.gov.ons.ctp.response.sample.service;
 import static org.mockito.Mockito.*;
 
 import java.util.UUID;
+import org.assertj.core.api.Fail;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.ons.ctp.response.sample.message.SampleSummaryActivationStatusPublisher;
 import uk.gov.ons.ctp.response.sample.representation.SampleSummaryActivationDTO;
+import uk.gov.ons.ctp.response.sample.representation.SampleSummaryStatusDTO;
 
 /** tests */
 @RunWith(MockitoJUnitRunner.class)
@@ -20,6 +23,7 @@ public class SampleSummaryActivationServiceTest {
 
   @Mock private SampleSummaryDistributionService sampleSummaryDistributionService;
   @Mock private SampleSummaryEnrichmentService sampleSummaryEnrichmentService;
+  @Mock private SampleSummaryActivationStatusPublisher sampleSummaryActivationStatusPublisher;
 
   // class under test
   @InjectMocks private SampleSummaryActivationService sampleSummaryActivationService;
@@ -46,7 +50,8 @@ public class SampleSummaryActivationServiceTest {
 
   @Test(expected = SampleSummaryActivationException.class)
   public void testActivateSampleSummaryFromPubsubCorrectEnrichExceptionThrown()
-      throws UnknownSampleSummaryException, SampleSummaryActivationException {
+      throws UnknownSampleSummaryException, SampleSummaryActivationException,
+          NoSampleUnitsInSampleSummaryException {
     SampleSummaryActivationDTO sampleSummaryActivation = new SampleSummaryActivationDTO();
     sampleSummaryActivation.setSampleSummaryId(SAMPLE_SUMMARY_ID);
     sampleSummaryActivation.setCollectionExerciseId(COLLECTION_EXERCISE_ID);
@@ -57,34 +62,98 @@ public class SampleSummaryActivationServiceTest {
         .thenThrow(new UnknownSampleSummaryException());
 
     sampleSummaryActivationService.activateSampleSummaryFromPubsub(sampleSummaryActivation);
-    verify(sampleSummaryEnrichmentService, times(1))
-        .enrich(SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID);
-    verify(sampleSummaryActivationService, times(1))
-        .sendEnrichStatusToCollectionExercise(COLLECTION_EXERCISE_ID, false);
-    verify(sampleSummaryActivationService, never())
-        .sendDistributeStatusToCollectionExercise(COLLECTION_EXERCISE_ID, anyBoolean());
   }
 
-  @Test(expected = SampleSummaryActivationException.class)
-  public void testActivateSampleSummaryFromPubsubCorrectDistributeExceptionThrown()
-      throws UnknownSampleSummaryException, SampleSummaryActivationException,
-          NoSampleUnitsInSampleSummaryException {
+  @Test()
+  public void testActivateSampleSummaryFromPubsubCorrectNumberOfCallsOnEnrichFailure()
+      throws UnknownSampleSummaryException, NoSampleUnitsInSampleSummaryException {
     SampleSummaryActivationDTO sampleSummaryActivation = new SampleSummaryActivationDTO();
     sampleSummaryActivation.setSampleSummaryId(SAMPLE_SUMMARY_ID);
     sampleSummaryActivation.setCollectionExerciseId(COLLECTION_EXERCISE_ID);
     sampleSummaryActivation.setSurveyId(SURVEY_ID);
+
+    when(sampleSummaryEnrichmentService.enrich(
+            SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID))
+        .thenThrow(new UnknownSampleSummaryException());
+    try {
+      sampleSummaryActivationService.activateSampleSummaryFromPubsub(sampleSummaryActivation);
+
+      // Will only get here if the above method doesn't throw an exception
+      Fail.fail("Exception should've been thrown");
+    } catch (SampleSummaryActivationException e) {
+      verify(sampleSummaryEnrichmentService, times(1))
+          .enrich(SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID);
+      verify(sampleSummaryDistributionService, never()).distribute(SAMPLE_SUMMARY_ID);
+      SampleSummaryStatusDTO sampleSummaryStatusDTO = new SampleSummaryStatusDTO();
+      sampleSummaryStatusDTO.setCollectionExerciseId(COLLECTION_EXERCISE_ID);
+      sampleSummaryStatusDTO.setEvent(SampleSummaryStatusDTO.Event.ENRICHED);
+      sampleSummaryStatusDTO.setSuccessful(false);
+      verify(sampleSummaryActivationStatusPublisher, times(1))
+          .sendSampleSummaryActivation(sampleSummaryStatusDTO);
+
+      sampleSummaryStatusDTO.setEvent(SampleSummaryStatusDTO.Event.DISTRIBUTED);
+      verify(sampleSummaryActivationStatusPublisher, never())
+          .sendSampleSummaryActivation(sampleSummaryStatusDTO);
+    }
+  }
+
+  @Test(expected = SampleSummaryActivationException.class)
+  public void testActivateSampleSummaryFromPubsubCorrectDistributeExceptionThrown()
+      throws SampleSummaryActivationException, NoSampleUnitsInSampleSummaryException,
+          UnknownSampleSummaryException {
+    SampleSummaryActivationDTO sampleSummaryActivation = new SampleSummaryActivationDTO();
+    sampleSummaryActivation.setSampleSummaryId(SAMPLE_SUMMARY_ID);
+    sampleSummaryActivation.setCollectionExerciseId(COLLECTION_EXERCISE_ID);
+    sampleSummaryActivation.setSurveyId(SURVEY_ID);
+
+    when(sampleSummaryEnrichmentService.enrich(
+            SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID))
+        .thenReturn(true);
 
     doThrow(new NoSampleUnitsInSampleSummaryException())
         .when(sampleSummaryDistributionService)
         .distribute(SAMPLE_SUMMARY_ID);
 
     sampleSummaryActivationService.activateSampleSummaryFromPubsub(sampleSummaryActivation);
-    verify(sampleSummaryEnrichmentService, times(1))
-        .enrich(SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID);
-    verify(sampleSummaryActivationService, times(1))
-        .sendEnrichStatusToCollectionExercise(COLLECTION_EXERCISE_ID, true);
-    verify(sampleSummaryDistributionService, times(1)).distribute(SAMPLE_SUMMARY_ID);
-    verify(sampleSummaryActivationService, times(1))
-        .sendDistributeStatusToCollectionExercise(COLLECTION_EXERCISE_ID, false);
+  }
+
+  @Test()
+  public void testActivateSampleSummaryFromPubsubCorrectNumberOfCallsOnDistributeFailure()
+      throws UnknownSampleSummaryException, NoSampleUnitsInSampleSummaryException {
+    SampleSummaryActivationDTO sampleSummaryActivation = new SampleSummaryActivationDTO();
+    sampleSummaryActivation.setSampleSummaryId(SAMPLE_SUMMARY_ID);
+    sampleSummaryActivation.setCollectionExerciseId(COLLECTION_EXERCISE_ID);
+    sampleSummaryActivation.setSurveyId(SURVEY_ID);
+
+    when(sampleSummaryEnrichmentService.enrich(
+            SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID))
+        .thenReturn(true);
+
+    doThrow(new NoSampleUnitsInSampleSummaryException())
+        .when(sampleSummaryDistributionService)
+        .distribute(SAMPLE_SUMMARY_ID);
+
+    try {
+      sampleSummaryActivationService.activateSampleSummaryFromPubsub(sampleSummaryActivation);
+
+      // Will only get here if the above method doesn't throw an exception
+      Fail.fail("Exception should've been thrown");
+    } catch (SampleSummaryActivationException e) {
+      verify(sampleSummaryEnrichmentService, times(1))
+          .enrich(SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID);
+      verify(sampleSummaryDistributionService, times(1)).distribute(SAMPLE_SUMMARY_ID);
+
+      SampleSummaryStatusDTO sampleSummaryStatusDTO = new SampleSummaryStatusDTO();
+      sampleSummaryStatusDTO.setCollectionExerciseId(COLLECTION_EXERCISE_ID);
+      sampleSummaryStatusDTO.setEvent(SampleSummaryStatusDTO.Event.ENRICHED);
+      sampleSummaryStatusDTO.setSuccessful(true);
+      verify(sampleSummaryActivationStatusPublisher, times(1))
+          .sendSampleSummaryActivation(sampleSummaryStatusDTO);
+
+      sampleSummaryStatusDTO.setSuccessful(false);
+      sampleSummaryStatusDTO.setEvent(SampleSummaryStatusDTO.Event.DISTRIBUTED);
+      verify(sampleSummaryActivationStatusPublisher, times(1))
+          .sendSampleSummaryActivation(sampleSummaryStatusDTO);
+    }
   }
 }
