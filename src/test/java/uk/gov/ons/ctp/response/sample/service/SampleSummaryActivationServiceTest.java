4242a2e8d96fbfe618036bfa5019030c3e2bd03e
Adam Mann <manny160389@hotmail.com>
2021-08-18 17:03:18 +0100

    Tidy up

--- src/test/java/uk/gov/ons/ctp/response/sample/service/SampleSummaryActivationServiceTest.java
+++ src/test/java/uk/gov/ons/ctp/response/sample/service/SampleSummaryActivationServiceTest.java
@@ -1,26 +1,14 @@
 package uk.gov.ons.ctp.response.sample.service;
 
+import static org.mockito.Mockito.*;
+
+import java.util.UUID;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import uk.gov.ons.ctp.response.sample.domain.model.SampleSummary;
-import uk.gov.ons.ctp.response.sample.domain.model.SampleUnit;
-import uk.gov.ons.ctp.response.sample.domain.repository.SampleSummaryRepository;
-import uk.gov.ons.ctp.response.sample.message.SampleSummaryActivationStatusPublisher;
-import uk.gov.ons.ctp.response.sample.message.SampleUnitPublisher;
 import uk.gov.ons.ctp.response.sample.representation.SampleSummaryActivationDTO;
-import uk.gov.ons.ctp.response.sample.representation.SampleUnitParentDTO;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
 
 /** tests */
 @RunWith(MockitoJUnitRunner.class)
@@ -38,50 +26,65 @@ public class SampleSummaryActivationServiceTest {
 
   @Test
   public void testActivateSampleSummaryFromPubsub()
-          throws SampleSummaryActivationException, UnknownSampleSummaryException, NoSampleUnitsInSampleSummaryException {
+      throws SampleSummaryActivationException, UnknownSampleSummaryException,
+          NoSampleUnitsInSampleSummaryException {
 
     SampleSummaryActivationDTO sampleSummaryActivation = new SampleSummaryActivationDTO();
     sampleSummaryActivation.setSampleSummaryId(SAMPLE_SUMMARY_ID);
     sampleSummaryActivation.setCollectionExerciseId(COLLECTION_EXERCISE_ID);
     sampleSummaryActivation.setSurveyId(SURVEY_ID);
 
-    when(sampleSummaryEnrichmentService.enrich(SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID)).thenReturn(true);
+    when(sampleSummaryEnrichmentService.enrich(
+            SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID))
+        .thenReturn(true);
 
     sampleSummaryActivationService.activateSampleSummaryFromPubsub(sampleSummaryActivation);
-    verify(sampleSummaryEnrichmentService, times(1)).enrich(SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID);
+    verify(sampleSummaryEnrichmentService, times(1))
+        .enrich(SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID);
     verify(sampleSummaryDistributionService, times(1)).distribute(SAMPLE_SUMMARY_ID);
   }
 
   @Test(expected = SampleSummaryActivationException.class)
   public void testActivateSampleSummaryFromPubsubCorrectEnrichExceptionThrown()
-          throws UnknownSampleSummaryException, SampleSummaryActivationException {
+      throws UnknownSampleSummaryException, SampleSummaryActivationException {
     SampleSummaryActivationDTO sampleSummaryActivation = new SampleSummaryActivationDTO();
     sampleSummaryActivation.setSampleSummaryId(SAMPLE_SUMMARY_ID);
     sampleSummaryActivation.setCollectionExerciseId(COLLECTION_EXERCISE_ID);
     sampleSummaryActivation.setSurveyId(SURVEY_ID);
 
-    when(sampleSummaryEnrichmentService.enrich(SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID)).thenThrow(new UnknownSampleSummaryException());
+    when(sampleSummaryEnrichmentService.enrich(
+            SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID))
+        .thenThrow(new UnknownSampleSummaryException());
 
     sampleSummaryActivationService.activateSampleSummaryFromPubsub(sampleSummaryActivation);
-    verify(sampleSummaryEnrichmentService, times(1)).enrich(SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID);
-    verify(sampleSummaryActivationService, times(1)).sendEnrichStatusToCollectionExercise(COLLECTION_EXERCISE_ID, false);
-    verify(sampleSummaryActivationService, never()).sendDistributeStatusToCollectionExercise(COLLECTION_EXERCISE_ID, anyBoolean());
+    verify(sampleSummaryEnrichmentService, times(1))
+        .enrich(SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID);
+    verify(sampleSummaryActivationService, times(1))
+        .sendEnrichStatusToCollectionExercise(COLLECTION_EXERCISE_ID, false);
+    verify(sampleSummaryActivationService, never())
+        .sendDistributeStatusToCollectionExercise(COLLECTION_EXERCISE_ID, anyBoolean());
   }
 
   @Test(expected = SampleSummaryActivationException.class)
   public void testActivateSampleSummaryFromPubsubCorrectDistributeExceptionThrown()
-          throws UnknownSampleSummaryException, SampleSummaryActivationException, NoSampleUnitsInSampleSummaryException {
+      throws UnknownSampleSummaryException, SampleSummaryActivationException,
+          NoSampleUnitsInSampleSummaryException {
     SampleSummaryActivationDTO sampleSummaryActivation = new SampleSummaryActivationDTO();
     sampleSummaryActivation.setSampleSummaryId(SAMPLE_SUMMARY_ID);
     sampleSummaryActivation.setCollectionExerciseId(COLLECTION_EXERCISE_ID);
     sampleSummaryActivation.setSurveyId(SURVEY_ID);
 
-    doThrow(new NoSampleUnitsInSampleSummaryException()).when(sampleSummaryDistributionService).distribute(SAMPLE_SUMMARY_ID);
+    doThrow(new NoSampleUnitsInSampleSummaryException())
+        .when(sampleSummaryDistributionService)
+        .distribute(SAMPLE_SUMMARY_ID);
 
     sampleSummaryActivationService.activateSampleSummaryFromPubsub(sampleSummaryActivation);
-    verify(sampleSummaryEnrichmentService, times(1)).enrich(SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID);
-    verify(sampleSummaryActivationService, times(1)).sendEnrichStatusToCollectionExercise(COLLECTION_EXERCISE_ID, true);
+    verify(sampleSummaryEnrichmentService, times(1))
+        .enrich(SURVEY_ID, SAMPLE_SUMMARY_ID, COLLECTION_EXERCISE_ID);
+    verify(sampleSummaryActivationService, times(1))
+        .sendEnrichStatusToCollectionExercise(COLLECTION_EXERCISE_ID, true);
     verify(sampleSummaryDistributionService, times(1)).distribute(SAMPLE_SUMMARY_ID);
-    verify(sampleSummaryActivationService, times(1)).sendDistributeStatusToCollectionExercise(COLLECTION_EXERCISE_ID, false);
+    verify(sampleSummaryActivationService, times(1))
+        .sendDistributeStatusToCollectionExercise(COLLECTION_EXERCISE_ID, false);
   }
 }
