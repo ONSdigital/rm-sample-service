391efe7141686d7d53c0d1d9c6973b539c6e438e
Adam Mann <manny160389@hotmail.com>
2021-08-24 17:17:59 +0100

    Revert breaking change

--- src/main/java/uk/gov/ons/ctp/response/sample/mapper/SampleUnitMapper.java
+++ src/main/java/uk/gov/ons/ctp/response/sample/mapper/SampleUnitMapper.java
@@ -2,15 +2,15 @@ package uk.gov.ons.ctp.response.sample.mapper;
 
 import ma.glasnost.orika.CustomMapper;
 import ma.glasnost.orika.MappingContext;
-import uk.gov.ons.ctp.response.sample.representation.SampleUnitDTO;
+import uk.gov.ons.ctp.response.sampleunit.definition.SampleUnit;
 
 public class SampleUnitMapper
-    extends CustomMapper<uk.gov.ons.ctp.response.sample.domain.model.SampleUnit, SampleUnitDTO> {
+    extends CustomMapper<uk.gov.ons.ctp.response.sample.domain.model.SampleUnit, SampleUnit> {
 
   @Override
   public void mapAtoB(
       uk.gov.ons.ctp.response.sample.domain.model.SampleUnit sampleUnit,
-      SampleUnitDTO sampleUnit2,
+      SampleUnit sampleUnit2,
       MappingContext context) {
     sampleUnit2.setFormType(sampleUnit.getFormType());
     sampleUnit2.setSampleUnitRef(sampleUnit.getSampleUnitRef());
