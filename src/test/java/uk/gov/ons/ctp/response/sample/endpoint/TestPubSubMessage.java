ee4f45961191cff269ed1e250d2e32fbab40656e
Adam Mann <manny160389@hotmail.com>
2021-08-25 11:52:54 +0100

    Forgot to format

--- src/test/java/uk/gov/ons/ctp/response/sample/endpoint/TestPubSubMessage.java
+++ src/test/java/uk/gov/ons/ctp/response/sample/endpoint/TestPubSubMessage.java
@@ -10,8 +10,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import uk.gov.ons.ctp.response.sample.utility.PubSubEmulator;
 import uk.gov.ons.ctp.response.sample.representation.SampleUnit;
+import uk.gov.ons.ctp.response.sample.utility.PubSubEmulator;
 
 public class TestPubSubMessage {
   private static final Logger log = LoggerFactory.getLogger(TestPubSubMessage.class);
