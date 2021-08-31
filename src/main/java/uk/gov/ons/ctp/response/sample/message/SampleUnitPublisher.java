514555bd8ffdbcca2f78f52a3802a033ea7b6b9a
Adam Mann <manny160389@hotmail.com>
2021-08-31 15:59:08 +0100

    Use spring integration for sample to case publish (#201)
    * Use spring integration for sample to case publish
    
    * auto patch increment
    
    * Slightly reorder logging
    
    Co-authored-by: ras-rm-pr-bot <matt.crocker+rasrm@gmail.com>

--- src/main/java/uk/gov/ons/ctp/response/sample/message/SampleUnitPublisher.java
+++ src/main/java/uk/gov/ons/ctp/response/sample/message/SampleUnitPublisher.java
@@ -4,15 +4,6 @@ import static net.logstash.logback.argument.StructuredArguments.kv;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.api.core.ApiFuture;
-import com.google.api.core.ApiFutureCallback;
-import com.google.api.core.ApiFutures;
-import com.google.api.gax.rpc.ApiException;
-import com.google.cloud.pubsub.v1.Publisher;
-import com.google.common.util.concurrent.MoreExecutors;
-import com.google.protobuf.ByteString;
-import com.google.pubsub.v1.PubsubMessage;
-import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,11 +17,12 @@ import uk.gov.ons.ctp.response.sampleunit.definition.SampleUnit;
 public class SampleUnitPublisher {
   private static final Logger log = LoggerFactory.getLogger(SampleUnitPublisher.class);
 
-  @Autowired private PubSub pubSub;
-
   @Autowired private ObjectMapper objectMapper;
 
   @Autowired private SampleSvcApplication.PubSubOutboundSampleUnitGateway samplePublisher;
+
+  @Autowired
+  private SampleSvcApplication.PubSubOutboundCaseNotificationGateway caseNotificationPublisher;
 
   /**
    * send sample to collection exercise via PubSub
@@ -53,53 +45,20 @@ public class SampleUnitPublisher {
   }
 
   /**
-   * Sends a sample unit to case via PubSub
+   * send sample to case via PubSub
    *
-   * @param sampleUnit A sample unit to be sent
+   * @param sampleUnit to be sent
    */
   public void sendSampleUnitToCase(SampleUnitParentDTO sampleUnit) {
-    log.debug(
-        "Entering sendSampleUnit",
-        kv("sample_unit_type", sampleUnit.getSampleUnitType()),
-        kv("sample_unit_ref", sampleUnit.getSampleUnitRef()));
     try {
+      log.info("Publishing message to PubSub", kv("sampleUnitRef", sampleUnit.getSampleUnitRef()));
       String message = objectMapper.writeValueAsString(sampleUnit);
-      ByteString data = ByteString.copyFromUtf8(message);
-      PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
-      Publisher publisher = pubSub.caseNotificationPublisher();
-      try {
-        log.info("Publishing message to PubSub");
-        ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
-        ApiFutures.addCallback(
-            messageIdFuture,
-            new ApiFutureCallback<>() {
-              @Override
-              public void onFailure(Throwable throwable) {
-                if (throwable instanceof ApiException) {
-                  ApiException apiException = ((ApiException) throwable);
-                  log.error(
-                      "SampleUnit publish sent failure to PubSub.",
-                      kv("error", apiException.getStatusCode().getCode()));
-                }
-                log.error("Error Publishing PubSub message", kv("message", message));
-              }
-
-              @Override
-              public void onSuccess(String messageId) {
-                // Once published, returns server-assigned message ids (unique within the topic)
-                log.info("SampleUnit publish sent successfully", kv("messageId", messageId));
-              }
-            },
-            MoreExecutors.directExecutor());
-      } finally {
-        publisher.shutdown();
-        pubSub.shutdown();
-      }
+      caseNotificationPublisher.sendToPubSub(message);
+      log.info(
+          "Sample unit to case publish sent successfully",
+          kv("sampleUnitRef", sampleUnit.getSampleUnitRef()));
     } catch (JsonProcessingException e) {
-      log.error("Error while sampleUnit can not be parsed.", kv("sampleUnit", sampleUnit));
-      throw new RuntimeException(e);
-    } catch (IOException e) {
-      log.error("PubSub Error while processing sample unit distribution", e);
+      log.error("Error converting sample unit to JSON", kv("sampleUnit", sampleUnit));
       throw new RuntimeException(e);
     }
   }
