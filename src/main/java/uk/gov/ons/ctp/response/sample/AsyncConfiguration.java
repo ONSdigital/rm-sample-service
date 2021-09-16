0fa9e8c9011f68193f287b6ff1bde983c065048e
Warren Bailey <warren@warrenbailey.net>
2021-09-16 11:33:30 +0100

    mvn fmt:format

--- src/main/java/uk/gov/ons/ctp/response/sample/AsyncConfiguration.java
+++ src/main/java/uk/gov/ons/ctp/response/sample/AsyncConfiguration.java
@@ -1,27 +1,25 @@
 package uk.gov.ons.ctp.response.sample;
 
+import static net.logstash.logback.argument.StructuredArguments.kv;
+
+import java.lang.reflect.Method;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 
-import java.lang.reflect.Method;
-
-import static net.logstash.logback.argument.StructuredArguments.kv;
-
 @Slf4j
 @Configuration
 public class AsyncConfiguration implements AsyncConfigurer {
 
-    @Override
-    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-        return new AsyncUncaughtExceptionHandler() {
+  @Override
+  public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+    return new AsyncUncaughtExceptionHandler() {
 
-            @Override
-            public void handleUncaughtException(Throwable ex, Method method, Object... params) {
-                log.error("Error during async call", ex, kv("called method", method.getName()));
-            }
-
-        };
-    }
+      @Override
+      public void handleUncaughtException(Throwable ex, Method method, Object... params) {
+        log.error("Error during async call", ex, kv("called method", method.getName()));
+      }
+    };
+  }
 }
