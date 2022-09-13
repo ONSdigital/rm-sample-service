2c9ed15a2a3daffc4f4e5b007bc122af09ae2115
Adam Mann <manny160389@hotmail.com>
2022-09-13 14:17:40 +0100

    Lint

--- src/test/java/uk/gov/ons/ctp/response/sample/endpoint/SampleEndpointUnitTest.java
+++ src/test/java/uk/gov/ons/ctp/response/sample/endpoint/SampleEndpointUnitTest.java
@@ -166,7 +166,9 @@ public class SampleEndpointUnitTest {
     when(sampleService.sampleSummaryStateCheck(any())).thenThrow(new NoSuchElementException());
 
     String url =
-        String.format("/samples/samplesummary/%s/check-and-transition-sample-summary-status", UUID.randomUUID());
+        String.format(
+            "/samples/samplesummary/%s/check-and-transition-sample-summary-status",
+            UUID.randomUUID());
 
     ResultActions actions = mockMvc.perform(get(url));
 
